96a78761e3918850848c414a981df4658949a674
Alex <107737661+Alexthemagician@users.noreply.github.com>
2025-08-18 00:55:41 -0400

    Added find by id mapping (temporary)

--- Back_End/i-heart-world-spring/i-heart-world/src/main/java/com/myfirstapp/i_heart_world/controllers/PostController.java
+++ Back_End/i-heart-world-spring/i-heart-world/src/main/java/com/myfirstapp/i_heart_world/controllers/PostController.java
@@ -1,9 +1,13 @@
 package com.myfirstapp.i_heart_world.controllers;
 
 import com.myfirstapp.i_heart_world.dao.UserPostRepository;
+import com.myfirstapp.i_heart_world.entity.User;
 import com.myfirstapp.i_heart_world.entity.UserPost;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/userPosts")
@@ -20,4 +24,17 @@ public class PostController {
         return userPostRepository.save(userPost);
     }
 
+    @GetMapping("/api/userPosts/postId")
+    public Long getPostId() {
+        System.out.println(userPost.getPostId());
+        return userPost.getPostId();
+
+    }
+
+    @GetMapping("/by-id")
+    public ResponseEntity<UserPost> getUserPostById(@RequestParam long id) {
+        Optional<UserPost> userPost = userPostRepository.findById(id);
+        return userPost.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
