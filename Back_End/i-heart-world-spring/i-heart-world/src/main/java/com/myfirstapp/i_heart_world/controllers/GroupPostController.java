c6295cb9f40e051311db11751064674d2af6686f
Alex <107737661+Alexthemagician@users.noreply.github.com>
2025-09-13 12:13:19 -0400

    Edited group post controller.

--- Back_End/i-heart-world-spring/i-heart-world/src/main/java/com/myfirstapp/i_heart_world/controllers/GroupPostController.java
+++ Back_End/i-heart-world-spring/i-heart-world/src/main/java/com/myfirstapp/i_heart_world/controllers/GroupPostController.java
@@ -5,24 +5,38 @@ import com.myfirstapp.i_heart_world.dao.UserPostRepository;
 import com.myfirstapp.i_heart_world.entity.GroupPost;
 import com.myfirstapp.i_heart_world.entity.UserPost;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/groupPosts")
-@CrossOrigin
+@CrossOrigin("http://localhost:4200")
 public class GroupPostController {
 
     @Autowired
     private GroupPostRepository groupPostRepository;
     private GroupPost groupPost;
 
-    @GetMapping
-    public java.util.List<GroupPost> getAllGroupPosts() {
-        return groupPostRepository.findAll();
-    }
-    @PostMapping
+
+    @PostMapping("/api/groupPosts")
     public GroupPost createGroupPost(@RequestBody GroupPost groupPost) {
         this.groupPost = groupPost;
         return groupPostRepository.save(groupPost);
+    }
+
+    @GetMapping("/api/userPosts/postId")
+    public Long getPostId() {
+        System.out.println(groupPost.getGroupPostId());
+        return groupPost.getGroupPostId();
+
+    }
+
+    @GetMapping("/by-id")
+    public ResponseEntity<GroupPost> getGroupPostById(@RequestParam long id) {
+        Optional<GroupPost> groupPost = groupPostRepository.findById(id);
+        return groupPost.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
